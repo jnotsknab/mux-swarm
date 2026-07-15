5c3fed130e1896f0fe5033f3ae239cd678f92bcb
Mux-Bud <Jtbtrichel+muxbud@gmail.com>
2026-07-15 02:30:37 -0500

    fix(process): remove unsafe Unix self-group sweep

---
+++ MuxSwarm.Tests/Tests/ProcessCleanupTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using MuxSwarm.Utils;
+using Xunit;
+
+namespace MuxSwarm.Tests.Tests;
+
+public class ProcessCleanupTests
+{
+    [Fact]
+    public void ProcessCleanup_HasNoPidDerivedProcessGroupSweep()
+    {
+        const BindingFlags AllMethods =
+            BindingFlags.Public | BindingFlags.NonPublic |
+            BindingFlags.Static | BindingFlags.Instance;
+
+        var methods = typeof(ProcessCleanup).GetMethods(AllMethods);
+
+        Assert.DoesNotContain(methods, method => method.Name == "KillOwnProcessTree");
+    }
+}
